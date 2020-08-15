f691e7d613283caec853224a5d732c4d51bb78d2
Sahith Nallapareddy <sahith.reddy@gmail.com>
2020-08-15 17:01:47 -0400

    using args

--- mod_updater/mod_updater.py
+++ mod_updater/mod_updater.py
@@ -1,6 +1,10 @@
 from .parser import parse_config
+import argparse
 
 def main():
-    sources = parse_config()
+    parser = argparse.ArgumentParser(description='config path')
+    parser.add_argument('--config', metavar='config_path', type=str, default="config.json")
+    args = parser.parse_args()
+    sources = parse_config(args.config)
     for source in sources:
         source.download()
